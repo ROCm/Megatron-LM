23b8578a52f3225cb44cbcac238a0d1a19cba6fa
sarthak arora <sarthak.arora@amd.com>
2025-09-08 01:31:25 -0500

    fix indentation

--- megatron/core/model_parallel_config.py
+++ megatron/core/model_parallel_config.py
@@ -22,10 +22,10 @@ class ModelParallelConfig:
     pipeline_model_parallel_size: int = 1
     """Inter-layer model parallelism. Splits transformer layers across GPU ranks."""
 
-   pipeline_model_parallel_comm_backend: Optional[str] = None
-   """Configuring backend option of pipeline parallel communication (e.g., nccl, ucc)
-      If None, the default backend will be used.
-   """
+    pipeline_model_parallel_comm_backend: Optional[str] = None
+    """Configuring backend option of pipeline parallel communication (e.g., nccl, ucc)
+       If None, the default backend will be used.
+    """
 
 
     virtual_pipeline_model_parallel_size: Optional[int] = None
