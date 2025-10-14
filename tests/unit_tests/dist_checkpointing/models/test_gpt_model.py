af4b892337d45d375ca16d445cff4f8ec87f6df3
Sudharshan Govindan <sugovind@amd.com>
2025-10-14 19:41:15 +0000

    test_sharded_state_dict_save_load TE error

--- tests/unit_tests/dist_checkpointing/models/test_gpt_model.py
+++ tests/unit_tests/dist_checkpointing/models/test_gpt_model.py
@@ -67,6 +67,7 @@ def initialize_gpt_model(seed, layer_spec_fn=gpt_te_spec, vocab_size=128, **conf
 class TestGPTModel:
     @pytest.mark.parametrize('src_layer_spec_fn', _spec_fn_list)
     @pytest.mark.parametrize('dst_layer_spec_fn', _spec_fn_list)
+    @pytest.mark.failing_on_rocm
     def test_sharded_state_dict_save_load(
         self,
         tmp_path_dist_ckpt: pathlib.Path,
